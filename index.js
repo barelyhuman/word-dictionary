327c632aac547e2ec3203ee85f1b3cdf6dc083c1
Siddharth Gelera <sidgroks@gmail.com>
2019-10-20 19:00:47 +0530

    spacing addition

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function main() {
     const actionArg = process.argv[2];
     const word = process.argv[3];
 
-    console.log(`Word:${word}`);
+    console.log(`Word: ${word}`);
 
     actionHandler(actionArg, word);
 }
