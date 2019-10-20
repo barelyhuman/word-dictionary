480c3edbde11a62b9ee536f38bb67ad17351d969
Siddharth Gelera <sidgroks@gmail.com>
2019-10-20 18:47:48 +0530

    antonym action addition

---
+++ actions/antonym.js
@@ -0,0 +1,20 @@
+const apiService = require('../utils/api.service');
+
+const relationshipTypeKey = 'antonym';
+
+module.exports = function (word) {
+    apiService.fetchRelatedWords(word)
+        .then(details => {
+            let antonyms = details.filter(item => item.relationshipType === relationshipTypeKey);
+            antonyms = antonyms.length > 0 ? antonyms[0].words : ["None Found"];
+            console.log(`Word:${word}`);
+            console.log("=====Antonyms=====");
+            antonyms.forEach(ant => {
+                console.log(ant);
+            });
+            console.log("-----xxxxxxxx-----");
+        })
+        .catch(err => {
+            throw err;
+        })
+}
